a079cb701dd70d73d0389835c84d399dfe792e66
jorika <gogoladzeg321@gmail.com>
2021-10-05 19:59:24 +0400

    Initialize project using Create React App

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,15 @@
+import Classes from './Home.module.css'
 const Home =()=> {
-    return <h1>home</h1>
+
+    return <header className={Classes.home_headers}>
+        <div className={Classes.home}>
+            <h1>home</h1>
+        </div>
+        <div className={Classes.links}>
+            <div>products</div>
+            <div>servises</div>
+            <div>contacts</div>
+        </div>
+    </header>
 }
 export default Home;
